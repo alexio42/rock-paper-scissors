1e4dcce73b455b8d512abb5e9f0fbe770c8bac20
alexio42 <116265425+alexio42@users.noreply.github.com>
2023-02-11 15:29:30 +0000

    Add index and js files
    Add html boiler plate, link to js file.
    Add psuedocode for game and prompt for user input

---
+++ script.js
@@ -0,0 +1,8 @@
+// function to get computer choice
+
+// Get choice from user
+
+let userInput = (prompt("Rock Paper or Scissors?", "")).toLowerCase();
+console.log(userInput);
+
+// function to take both inputs, compare, and declare winner
