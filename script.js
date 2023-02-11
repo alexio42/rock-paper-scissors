bbbef207f50ff5a744e71070e59a6caa0b121f26
alexio42 <116265425+alexio42@users.noreply.github.com>
2023-02-11 17:11:54 +0000

    Add playRound function
    playRound function takes player choice and computer choice,
    compares them, and declares a winner as an alert.

--- script.js
+++ script.js
@@ -7,12 +7,42 @@ function getComputerChoice() {
   return rockPaperScissors[random]
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 
 // Get choice from user
 
 let userInput = (prompt("Rock Paper or Scissors?", "")).toLowerCase();
-console.log(userInput);
+// console.log(userInput);
 
 // function to take both inputs, compare, and declare winner
+const playerSelection = userInput;
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Computer also chose " + playerSelection[0].toUpperCase() + playerSelection.slice(1) + ". It's a draw!"
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "scissors") {
+      return "Computer chose Scissors. You win!"
+    } else {
+      return "Computer chose Paper. You lose!"
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return "Computer chose Rock. You win!"
+    } else {
+      return "Computer chose Scissors. You lose!"
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "paper") {
+      return "Computer chose Paper. You win!"
+    } else {
+      return "Computer chose Rock. You lose!"
+    }
+  } else {
+    return "Please enter Rock, Paper, or Scissors"
+  }
+}
+
+alert(playRound(playerSelection, computerSelection));
